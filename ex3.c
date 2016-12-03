60ab6f0443a87f9892e5b96950a9a6e6f29895f3
hazrmard <ibrahim78786@gmail.com>
2016-12-03 01:37:57 -0600

    added ex3

---
+++ ex3.c
@@ -0,0 +1,14 @@
+// This program solves a system of linear equations of the form:
+//	A . x = b
+// Where A is the coefficient matrix, and x is the solution.
+// x and b are vectors of the same shape.
+// i.e. if A has shape m x n, then b and x are of shape: n x 1
+
+# include <petsc.h>
+
+
+int main(int argc, char** argv) {
+
+	
+	return 0;
+}
